24c78c742e821c9892f616b8653140a1ed415123
Matheus <matheushideki1234@hotmail.com>
2021-05-04 22:00:43 -0300

    parte 2

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Ana Luiza' },
+  { id: 2, name: 'Caique Cando' },
+  { id: 3, name: 'Roberto Dinamite' },
+  { id: 4, name: 'Fausto Silva' },
+  { id: 5, name: 'Cristiano Ronaldo' },
+  
+];
